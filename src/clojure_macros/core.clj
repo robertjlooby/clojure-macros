34bc507a3e5e10f1884fc93db8b4068d5b2dda13
robertjlooby <robertjlooby@gmail.com>
2013-08-07 11:40:57 -0500

    changed from loop to mutliple arity macro

--- src/clojure_macros/core.clj
+++ src/clojure_macros/core.clj
@@ -1,12 +1,9 @@
 (ns clojure_macros.core)
 
-(defmacro my-and [& args]
-  `(if (empty? '~args)
-     true
-     (loop [head# (eval (first '~args))
-            tail# (rest  '~args)]
-       (if (not head#)
-         head#
-         (if (empty? tail#)
-           head#
-           (recur (eval (first tail#)) (rest tail#)))))))
+(defmacro my-and 
+  ([] true)
+  ([arg] arg)
+  ([arg & args]
+    `(if ~arg
+       (my-and ~@args)
+       ~arg)))
