cd9537cf9dab5bd539387ecbc9ba3a26680c971e
robertjlooby <robertjlooby@gmail.com>
2013-08-08 14:44:24 -0500

    upgraded to 1.5

--- project.clj
+++ project.clj
@@ -2,7 +2,7 @@
   :description "Some macros written in clojure"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.4.0"]]
+  :dependencies [[org.clojure/clojure "1.5.0"]]
   :profiles {:dev {:dependencies [[speclj "2.5.0"]]}}
   :plugins [[speclj "2.5.0"]]
   :test-paths ["spec"])
